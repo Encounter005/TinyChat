69ac0b22150f2482219f7c2c2e37267a5dd86617
encounter005 <1624221574@qq.com>
2026-01-24 09:33:13 +0800

    feat(User): 使用新的结果类和错误码

--- Backend/src/service/UserService.h
+++ Backend/src/service/UserService.h
@@ -2,16 +2,24 @@
 #define USERSERVICE_H_
 
 #include <string>
+#include "common/result.h"
+#include "common/UserMessage.h"
+#include "common/const.h"
+
+
 
 class UserService {
 public:
-    static int Register(
+    static Result<int> Register(
         const std::string& user, const std::string& email,
         const std::string& passwd, const std::string& confirm,
         const std::string& varify_code);
 
-        static int ResetPass(const std::string&name, const std::string& email, const std::string& new_pass, const std::string &varify_code);
-
+    static Result<void> ResetPass(
+        const std::string& name, const std::string& email,
+        const std::string& new_pass, const std::string& varify_code);
+    static Result<UserInfo> Login(const std::string& email);
+    static Result<UserInfo> GetUserBase(int uid);
 };
 
 
